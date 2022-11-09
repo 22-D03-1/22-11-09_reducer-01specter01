c65bb6d5a4e15157d6254dbff9685642ea271205
Leo Volwahsen <volwahsenl@gmail.com>
2022-11-09 14:15:17 +0100

    hj

--- cooking-app/src/App.js
+++ cooking-app/src/App.js
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { useReducer } from "react";
+import recipes from "./components/recipes";
+
 
 function App() {
     return <div className="App">
 
-      
+
     </div>;
 }
 
