82bc39374c9133e4aa7b0df3e733bb2c23ced531
Leo Volwahsen <volwahsenl@gmail.com>
2022-11-09 14:44:04 +0100

    e

---
+++ cooking-app/src/Todos.js
@@ -0,0 +1,46 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { useReducer } from "react";
+import recipes from "./components/recipes";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "COMPLEATED":
+            return state.map((todo) => {
+                if (todo.id === action.id) {
+                    return { ...todo, compleate: !todo.compleate };
+                } else {
+                    return todo;
+                }
+            });
+        default:
+            return state;
+    }
+};
+
+function Todos() {
+    const [todos, dispatch] = useReducer(reducer, recipes);
+
+    const handleCompleate = (todo) => {
+        dispatch({ type: "COMPLEATE", id: todo.id });
+    };
+    return (
+        <>
+            {todos.map((todo) => (
+                <div key={todo.id}>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={todo.compleate}
+                            onChange={() => handleCompleate(todo)}
+                            value={recipes[1]}
+                        />
+                        {todo.title}
+                    </label>
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default Todos;
